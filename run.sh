4ff38dc501b1b266a9b6db6829db8812c4798805
Bowei Liu <liubowei@gmail.com>
2025-10-17 14:50:21 -0700

    bowei remember to do this

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export DOMAIN="$DOMAIN"
+
+docker-compose -f docker-compose-dev.yml up -d
+
 # Get the directory of this script:
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
